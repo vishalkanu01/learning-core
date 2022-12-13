db57d616dda4a0f90dd29be26fcfc6de29916e07
vishalkanu01 <vishalkanu01@gmail.com>
2022-12-13 11:50:17 +0545

    Create basic grid

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+
+import "./App.css";
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Box">
+        <div className="Display">
+          <h1>0</h1>
+        </div>
+        <div className="grid-container">
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+          <Button />
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import "./Button.css"
+
+
+const Button = () => {
+  return (
+    <div>
+      <button type="button">hola</button>
+    </div>
+  );
+};
+
+export default Button;
